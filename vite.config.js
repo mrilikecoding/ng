dd56e4be47a87f8f6a494b55388992df4f7a3394
Your Name <you@example.com>
2025-06-21 16:01:44 -0700

    add workflow for gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ng/",
   server: {
     host: "0.0.0.0", // Makes Vite listen on all interfaces
     port: 5173, // Keep the default port
