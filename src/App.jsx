1a3c2c31bb12468ed08c2f98a9e69882515e888c
Nathan Green <nathanrgreen@gmail.com>
2025-06-25 16:13:24 -0700

    add umami

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
 import Terminal from './components/Terminal/Terminal';
 import { ThemeProvider } from './contexts/ThemeContext';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const umamiId = import.meta.env.VITE_UMAMI_WEBSITE_ID;
+    const isProd = import.meta.env.PROD;
+
+    if (isProd && umamiId) {
+      const script = document.createElement('script');
+      script.defer = true;
+      script.src = 'https://cloud.umami.is/script.js';
+      script.setAttribute('data-website-id', umamiId);
+      document.head.appendChild(script);
+    }
+  }, []);
+
   return (
     <ThemeProvider>
       <Terminal />
